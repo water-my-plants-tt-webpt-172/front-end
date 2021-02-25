ca54b2c80c4ca93930ca2971e9d01f0b2b5c56d9
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-02-25 16:11:57 -0500

    Styling Guide added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SignLogin from './components/signlogin';
 import PlantGallery from './components/plantgallery'
+import PrivateRoute from './utils/PrivateRoute'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
         {/**Sign up Linked to from Marketing Page**/}
         <Route exact path ="/" component = { SignLogin }/>
-        <PrivateRoute path='/plants' component={PlantGallery}/>
+        <PrivateRoute path='/plants' component={ PlantGallery }/>
     </div>
     </Router>
   );
