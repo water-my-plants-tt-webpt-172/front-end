c668fdc33ab4971bf0df4de72d2439f26bb4243e
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-02-25 01:10:59 -0500

    Utils and App.js Router Set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import SignLogin from './components/signlogin';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/**Sign up Linked to from Marketing Page**/}
+        <Route path ="/" component = { SignLogin }/>
     </div>
+    </Router>
   );
 }
 
