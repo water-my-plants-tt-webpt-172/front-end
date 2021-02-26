3f1fdccee688ab875f369292f40014d54d43b6a7
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-02-25 23:49:04 -0500

    Added reducer cases and refactored actions

--- frontend/src/api/actions.js
+++ frontend/src/api/actions.js
@@ -1,84 +1,79 @@
 import axiosWithAuth from '../utils/authWithAxios'
 import axios from 'axios'
 
-export const PLANT_DATA_START = "PLANT_DATA_START";
-export const PLANT_DATA_SUCCESS = "PLANT_DATA_SUCCESS";
-export const PLANT_DATA_FAIL = "PLANT_DATA_FAIL";
-
-export const USER_LOGIN_START = "USER_LOGIN_START";
+export const API_ACTION_START = "API_ACTION_START"
+export const API_ACTION_FAIL = "API_ACTION_FAIL"
+export const PLANT_GET_SUCCESS = "PLANT_DATA_SUCCESS";
+export const PLANT_UPDATE_SUCCESS = "PLANT_UPDATE_SUCCESS";
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
-export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL";
-
-export const USER_SIGN_START = "USER_SIGN_START";
 export const USER_SIGN_SUCCESS = "USER_SIGN_SUCCESS";
-export const USER_SIGN_FAIL = "USER_SIGN_FAIL";
-
 export const UPDATE_ERROR = "UPDATE_ERROR";
 
 export const getPlants = () => dispatch => {
-    dispatch({ type: PLANT_DATA_START });
+    dispatch({ type: API_ACTION_START });
     axiosWithAuth()
         .get(someURL)
         .then(res =>
-            dispatch({ type: PLANT_DATA_SUCCESS, payload: res.somePayloadPoint}))
-        .catch(err => dispatch({ type: PLANT_DATA_FAIL, payload: err}))
+            dispatch({ type: PLANT_GET_SUCCESS, payload: res.somePayloadPoint}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 export const addPlant = (plant) => dispatch => {
-    dispatch({ type: PLANT_DATA_START });
+    dispatch({ type: API_ACTION_START });
     axiosWithAuth()
         .post(someURL, plant)
         .then(res =>
-            dispatch({ type: PLANT_DATA_SUCCESS, payload: res.somePayloadPoint}))
-        .catch(err => dispatch({ type: PLANT_DATA_FAIL, payload: err}))
+            dispatch({ type: PLANT_UPDATE_SUCCESS, payload: res.somePayloadPoint}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 export const editPlant = (plant) => dispatch => {
-    dispatch({ type: PLANT_DATA_START });
+    dispatch({ type: API_ACTION_START });
     axiosWithAuth()
         .put(someURL/plant.id, plant)
         .then(res =>
-            dispatch({ type: PLANT_DATA_SUCCESS, payload: res.somePayloadPoint}))
-        .catch(err => dispatch({ type: PLANT_DATA_FAIL, payload: err}))
+            dispatch({ type: PLANT_UPDATE_SUCCESS, payload: res.somePayloadPoint}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 export const deletePlant = (plant) => dispatch => {
-    dispatch({ type: PLANT_DATA_START });
+    dispatch({ type: API_ACTION_START });
     axiosWithAuth()
         .delete(someURL, plant)
         .then(res =>
             dispatch({ type: PLANT_DATA_SUCCESS, payload: res.somePayloadPoint}))
-        .catch(err => dispatch({ type: PLANT_DATA_FAIL, payload: err}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 export const userLogin = (loginInfo) => dispatch => {
-    dispatch({ type: USER_LOGIN_START});
+    dispatch({ type: API_ACTION_START});
     axios
         .post(someURL, loginInfo)
         .then(res => {
-            dispatch({type: USER_LOGIN_SUCCESS, payload : res.somePayloadPoint})
+            localStorage.setItem('token' , res.somePayloadPoint);
+            dispatch({type: USER_LOGIN_SUCCESS})
         })
-        .catch(err => dispatch({ type: USER_LOGIN_FAIL, payload: err}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 export const userSignUp = (signUpInfo) => dispatch => {
-    dispatch({ type: USER_SIGN_START });
+    dispatch({ type: API_ACTION_START });
     axios
         .post(someURL, signUpInfo)
         .then(res => {
             dispatch({type: USER_SIGN_SUCCESS, payload : res.somePayloadPoint})
         })
-        .catch(err => dispatch({ type: USER_SIGN_FAIL, payload: err}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 export const userEdit = (userEdit) => dispatch => {
-    dispatch({ type: USER_SIGN_START });
-    axios
+    dispatch({ type: API_ACTION_START });
+    authWithAxios()
         .put(someURL/userEdit.username, userEdit)
         .then(res => {
             dispatch({type: USER_SIGN_SUCCESS, payload : res.somePayloadPoint})
         })
-        .catch(err => dispatch({ type: USER_SIGN_FAIL, payload: err}))
+        .catch(err => dispatch({ type: API_ACTION_FAIL, payload: err}))
 }
 
 
--- frontend/src/api/rootreducer.js
+++ frontend/src/api/rootreducer.js
@@ -1,18 +1,64 @@
 import * as actions from './actions'
 
 export const initialState = {
-    isLoading : false,
-    user : {
-        username : '',
-        password : '',
-        telephone : ''
+    isLoading: false,
+    user: {
+        username: '',
+        password: '',
+        telephone: ''
     },
-    plants : [],
-    error : ''
+    plants: [],
+    error: '',
+    success: ''
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        
+        case actions.API_ACTION_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: '',
+                success: ''
+            };
+        case actions.API_ACTION_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            };
+        case actions.PLANT_GET_SUCCESS:
+            return {
+                ...state,
+                plants: action.payload,
+                isLoading: false,
+                error: ''
+            };
+        case actions.PLANT_UPDATE_SUCCESS:
+            actions.getPlants();
+            return {
+                ...state,
+                isLoading: false,
+                success: action.payload,
+                error: ''
+            };
+        case actions.USER_LOGIN_SUCCESS:
+            return {
+                ...state,
+                password: '',
+                isLoading: false,
+                error: ''
+            }
+        case actions.USER_SIGN_SUCCESS:
+            //Maybe have it auto sign in users after sign up
+            return {
+                ...state,
+                isLoading: false,
+                username: '',
+                password: '',
+                error: ''
+            }
     }
-} 
+}
+
+export default reducer;
