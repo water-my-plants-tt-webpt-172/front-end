7d72d1b7d40fe36b740516616ff8bd1b28c98b61
crash138 <67919714+sarah-herren@users.noreply.github.com>
2021-02-25 23:59:43 -0600

    created dummy data for plantGallery **needs styling

---
+++ frontend/src/components/plantgallery.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PlantCard from './plantCard';
+
+class plantGallery extends Component {
+    static defaultProps = {
+        usersPlants: [
+            { id: 1, nickname: 'Pearly', species: 'curio' },
+            { id: 2, nickname: 'Grumpy', species: 'cactus' },
+            { id: 3, nickname: 'Spike', species: 'air' }
+        ]
+    };
+
+    render() {
+        return (
+            <div>
+                {this.props.usersPlants.map((plant) => (
+                    <PlantCard nickname={plant.nickname} species={plant.species} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default plantGallery;
